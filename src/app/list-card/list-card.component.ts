3ba79313229227b22aa8c14d6245e4690f07e2df
Jenny <jenny.m.lewis@hotmail.co.uk>
2022-08-17 16:11:13 +0100

    plan to display lists dependent on car title

--- src/app/list-card/list-card.component.ts
+++ src/app/list-card/list-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnChanges, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ListCardService } from '../services/list-card.service';
 
 
